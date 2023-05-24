49b9b08e5e75fe0d19a44208112ae949e87617a4
slvally <fieshidiq@gmail.com>
2023-05-24 17:42:24 +0700

    final

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -31,50 +31,9 @@ async function createUsers(request){
 
   // Get request Body
   const { username, email, password, birthdate, gender, height, weight } = request.body
-
-  // const imageUrl = request.file.path
-  
-  // Error message
-  // if (!title || !description) {
-  //   let message = ""
-    
-  //     if (!title ) {
-  //       message += ", title"
-  //     }
-      
-  //     // if (!imageUrl) {
-
-  //     //   message += ", imageUrl"
-  //     // }
-
-  //     if (!description) {
-  //       message += ", description"
-  //     }
-    
-  //     return { 
-  //       status: 'Failed',
-  //       code: 400,
-  //       message: `Failed creating gallery${message} is empty!`
-  //     }
-  //   }
     
     try {
-      
-      
-      // let imageUrl = null
-      // let filename = null
-
-      // if (request.file) {
-      //   imageUrl = request.file.path
-      //   filename = request.file.filename
-      //   // console.log(request.file)
-      // }
-
-      // set default imageUrl if empty
-      // const defaultImageUrl = 'https://res.cloudinary.com/dp7yp5kgv/image/upload/v1684334844/galleries/map_uqx7qx.jpg';
-      // const finalImageUrl = imageUrl || defaultImageUrl;
-      
-      // Create new gallery record using the Galleries model
+      // Create new users record using the Galleries model
       const newUsers = await Users.create({
         id: uuidv4(),
         username,
@@ -88,7 +47,7 @@ async function createUsers(request){
         updatedAt: new Date()
       });
 
-      // Return the newly created gallery in the response
+      // Return the newly users gallery in the response
       return {
         status: "success",
         code: 201,
@@ -110,14 +69,3 @@ export default {
   getMultiple,
   createUsers
 }
-
-
-// {
-//   "username":"test",
-//   "email":"test@gmail.com",
-//   "password":"test",
-//   "birthdate":"2002-05-05",
-//   "gender":"male",
-//   "height":1.0,
-//   "weight":1.0,
-// }
