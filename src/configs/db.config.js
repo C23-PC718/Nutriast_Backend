cfbd95ae472bc5ff13d0e1be82d7c0b69fd862a6
slvally <fieshidiq@gmail.com>
2023-05-24 16:15:08 +0700

    commit

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,17 +1,20 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('nutriast', 'root', 'nutriastapi28', {
-  host: '34.101.200.2',
-  dialect: 'mysql'
+// dbname, username, password
+const db = new Sequelize("", "", "", {
+  host: "34.101.200.2",
+  dialect: "mysql",
 });
 
+
 // Sinkron table pada db dari model
-/* db.sync({alter: true})
-  .then(() => {
-    console.log('Tabel berhasil di sinkronisasi');
-  })
-  .catch((error) => {
-    console.error('Terjadi kesalahan:', error);
-  }); */
+
+// db.sync({ alter: true })
+//   .then(() => {
+//     console.log("Tabel berhasil di sinkronisasi");
+//   })
+//   .catch((error) => {
+//     console.error("Terjadi kesalahan:", error);
+//   });
 
 export default db;
