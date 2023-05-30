6b5041ae845d2c79f76ed61161620f3ff72fbab7
slvally <fieshidiq@gmail.com>
2023-05-30 20:11:41 +0700

    env

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,9 +1,11 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
 // dbname, username, password
-const db = new Sequelize("", "", "", {
-  host: "",
-  dialect: "mysql",
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT
 });
 
 
