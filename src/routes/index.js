097b065f7577659a30a81e39bd47a168cf4abffe
slvally <fieshidiq@gmail.com>
2023-05-30 16:07:42 +0700

    working on regis

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import usersController from '../controllers/users.controller.js';
+import usersController from '../controllers/intakeusers.controller';
 
 const router =  express.Router();
 
 /* users */
 router.post('/users', usersController.create);
 router.get('/users', usersController.get);
+
+router.post('/intakeusers', usersController.create);
+router.get('/intakeusers', usersController.get);
 
 // {
 //   "username":"rafi",
--- src/services/users.service.js
+++ src/services/users.service.js
@@ -4,11 +4,8 @@ import { Users } from "../models/users.model.js";
 // import cloudinaryConfig from '../configs/cloudinary.config.js';
 
 async function getMultiple(){
-  
   try {
-
     const dbResult = await Users.findAll();
-
     // Return the mapped in the response
     return {
       status: "success", 
@@ -16,7 +13,6 @@ async function getMultiple(){
       message : 'Fetching users successfully!',
       data : dbResult
     }
-    
   } catch (err) {
     console.error(err);
     return {
@@ -28,10 +24,8 @@ async function getMultiple(){
 }
 
 async function createUsers(request){
-
   // Get request Body
   const { username, email, password, birthdate, gender, height, weight } = request.body
-    
     try {
       // Create new users record using the model
       const newUsers = await Users.create({
@@ -46,7 +40,6 @@ async function createUsers(request){
         createdAt: new Date(),
         updatedAt: new Date()
       });
-
       // Return the newly users in the response
       return {
         status: "success",
@@ -54,7 +47,6 @@ async function createUsers(request){
         message: 'new user created successfully!',
         data: newUsers
       }
-    
   } catch (err) {
     console.error(err);
     return {
@@ -64,6 +56,48 @@ async function createUsers(request){
     }
   }
 }
+
+/*
+async function registerUsers(request){
+
+  var responseError = new ResponseClass.ErrorResponse()
+  var responseSuccess = new ResponseClass.SuccessResponse()
+
+  if(!request.email || !request.password || !request.email || !request.birthdate || !request.gender || !request.height || !request.weight)
+
+  // Get request Body
+  const { username, email, password, birthdate, gender, height, weight } = request.body
+    try {
+      // Create new users record using the model
+      const newUsers = await Users.create({
+        id: uuidv4(),
+        username,
+        email,
+        password, 
+        birthdate, 
+        gender, 
+        height, 
+        weight,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      });
+      // Return the newly users in the response
+      return {
+        status: "success",
+        code: 201,
+        message: 'new user created successfully!',
+        data: newUsers
+      }
+  } catch (err) {
+    console.error(err);
+    return {
+      status: "Failed", 
+      code : 400,
+      message : 'Error creating user!'
+    }
+  }
+}
+*/
 
 export default {
   getMultiple,
